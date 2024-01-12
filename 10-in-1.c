2ad8be8642af52cb437832a876886f5c6adfadaa
Shubham Panda <153392469+pandasuvm@users.noreply.github.com>
2024-01-12 23:54:18 +0530

    Update 10-in-1.c

--- 10-in-1.c
+++ 10-in-1.c
@@ -20,7 +20,6 @@ int reverseNumber(int num) {
 
 int main() {
     int choice;
-
     do {
         printf("\nMenu:\n");
         printf("1. Sum\n");
@@ -37,8 +36,90 @@ int main() {
         scanf("%d", &choice);
 
         switch (choice) {
-            // ... (existing cases)
-
+            case 1: {
+                int num1, num2;
+                printf("Enter two numbers: ");
+                scanf("%d %d", &num1, &num2);
+                printf("Sum: %d\n", num1 + num2);
+                break;
+            }
+            case 2: {
+                int num1, num2;
+                printf("Enter two numbers: ");
+                scanf("%d %d", &num1, &num2);
+                printf("Multiplication: %d\n", num1 * num2);
+                break;
+            }
+            case 3: {
+                int num1, num2;
+                printf("Enter two numbers: ");
+                scanf("%d %d", &num1, &num2);
+                if (num2 != 0)
+                    printf("Division: %f\n", (float)num1 / num2);
+                else
+                    printf("Cannot divide by zero!\n");
+                break;
+            }
+            case 4: {
+                int num1, num2, temp;
+                printf("Enter two numbers: ");
+                scanf("%d %d", &num1, &num2);
+                temp = num1;
+                num1 = num2;
+                num2 = temp;
+                printf("Values swapped: %d %d\n", num1, num2);
+                break;
+            }
+            case 5: {
+                int num;
+                printf("Enter a number: ");
+                scanf("%d", &num);
+                if (isPrime(num))
+                    printf("%d is a prime number.\n", num);
+                else
+                    printf("%d is not a prime number.\n", num);
+                break;
+            }
+            case 6: {
+                int num;
+                printf("Enter a number: ");
+                scanf("%d", &num);
+                if (num == reverseNumber(num))
+                    printf("%d is a palindrome number.\n", num);
+                else
+                    printf("%d is not a palindrome number.\n", num);
+                break;
+            }
+            case 7: {
+                int n, a = 0, b = 1, nextTerm;
+                printf("Enter the number of terms: ");
+                scanf("%d", &n);
+                printf("Fibonacci Series: ");
+                for (int i = 1; i <= n; i++) {
+                    printf("%d, ", a);
+                    nextTerm = a + b;
+                    a = b;
+                    b = nextTerm;
+                }
+                printf("\n");
+                break;
+            }
+            case 8: {
+                double angle;
+                printf("Enter an angle in degrees: ");
+                scanf("%lf", &angle);
+                printf("Sin: %.4lf\n", sin(angle * M_PI / 180));
+                printf("Cos: %.4lf\n", cos(angle * M_PI / 180));
+                printf("Tan: %.4lf\n", tan(angle * M_PI / 180));
+                break;
+            }
+            case 9: {
+                int num;
+                printf("Enter a number: ");
+                scanf("%d", &num);
+                printf("Reversed number: %d\n", reverseNumber(num));
+                break;
+            }
             case 0: {
                 printf("Exiting the program.\n");
                 break;
